a078aa862a9b9c07583ecfa09e4deb24a4b5c487
Sumant Singh <singhsumant581@gmail.com>
2023-01-31 18:54:05 +0530

    app.get

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -2,6 +2,12 @@ const express = require("express");
 
 const app = express();
 
+app.get("/", function(request,response){
+    // console.log(request);
+    // response.send("Hello");
+    response.send("<h1>Hello World</h1>");
+});
+
 app.listen(3000,function(){
     console.log("Server started on port 3000");
 });
