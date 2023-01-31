7e2a33bbf7eeb13da39ce698dc56894cb52b749a
Sumant Singh <singhsumant581@gmail.com>
2023-01-31 18:08:29 +0530

    modified node files

--- intro-to-node/index.js
+++ intro-to-node/index.js
@@ -1,5 +1,8 @@
 var superheroes = require("superheroes");
+var supervillains = require('supervillains');
 
 var mySuperheroName = superheroes.random();
+var mySupervillains = supervillains.random();
 
-console.log(mySuperheroName);
+console.log("Superhero: " + mySuperheroName);
+console.log("Villain: " + mySupervillains);
