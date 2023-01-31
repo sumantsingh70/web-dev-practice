c02a184cd9d03a12e170669bd006df3061473464
Sumant Singh <singhsumant581@gmail.com>
2023-01-31 20:22:00 +0530

    My new Calculator project using basic web application

--- my-express-server/server.js
+++ my-express-server/server.js
@@ -1,3 +1,4 @@
+const { request } = require("express");
 const express = require("express");
 
 const app = express();
@@ -7,6 +8,20 @@ app.get("/", function(request,response){
     // response.send("Hello");
     response.send("<h1>Hello World</h1>");
 });
+
+app.get("/contact" , function(req,res){
+    res.send("contact me at : singhsumant581@gmail.com");
+});
+
+app.get("/about" , function(req,res){
+    res.send("My name is Sumant Singh and I am a student at Punjabi University Patiala.");
+});
+
+app.get("/hobbies", function(req,res){
+    res.send("<ul><li>coffee</li><li>code</li></ul>")
+})
+
+
 
 app.listen(3000,function(){
     console.log("Server started on port 3000");
