61951184e95b767bea5769ad458ed8763fb1eb2b
Sumant Singh <singhsumant581@gmail.com>
2023-01-31 22:50:45 +0530

    added some more files

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -15,6 +15,17 @@ app.post("/", function(req,res){
     res.send("The result of calculation is: " + result);
 });
 
+app.get("/bmicalculator", function(req,res){
+    res.sendFile(__dirname + "/bmicalculator.html");
+});
+
+app.post("/bmicalculator", function(req,res){
+    var height = parseFloat(req.body.height);
+    var weight = parseFloat(req.body.weight);
+    var bmi = weight/(height*height);
+    res.send("Your BMI is:" + bmi);
+})
+
 app.listen(3000, function () {
 	console.log("server is running on port 3000.");
 });
