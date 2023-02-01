7015d40240bbe48b4eee88edfcaadc96c78bacd6
Sumant Singh <singhsumant581@gmail.com>
2023-02-01 13:53:38 +0530

    add climate icon and published over website rather than on server

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -4,20 +4,30 @@ const https = require("https");
 const app = express();
 
 app.get("/", function (req, res) {
+
+    
 	const url =
-		"https://api.openweathermap.org/data/2.5/weather?q=London&appid=831deb81a5ca396ab9e3e69a6f509818&unit=metric";
+		"https://api.openweathermap.org/data/2.5/weather?q=London&appid=831deb81a5ca396ab9e3e69a6f509818&units=metric";
 	https.get(url, function (response) {
 		console.log(response.statusCode);
 
-        response.on("data", function(data){
-            const weatherData = JSON.parse(data);
-            const temp = weatherData.main.temp;
-            const weatherDescription = weatherData.weather[0].description;
-            console.log(temp);
-            console.log(weatherDescription);
-        }); 
+		response.on("data", function (data) {
+			const weatherData = JSON.parse(data);
+			const temp = weatherData.main.temp;
+			const weatherDescription = weatherData.weather[0].description;
+
+			const icon = weatherData.weather[0].icon;
+			const imageURL = "http://openweathermap.org/img/wn/" + icon + "@2x.png";
+
+			res.write("<p>The weather is currently " + weatherDescription + "</p>");
+			res.write(
+				"<h1>The temperature in London is " + temp + " degree Celcius.</h1>"
+			);
+
+			res.write("<img src=" + imageURL + ">");
+			res.send();
+		});
 	});
-	res.send("Server is up and running.");
 });
 
 app.listen(3000, function () {
